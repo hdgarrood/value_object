547714f4928a8451d6ef4988139e9d2bc58c6053
Harry Garrood <hdgarrood@gmail.com>
2012-10-02 00:06:00 +0100

    add source

--- lib/value_object.rb
+++ lib/value_object.rb
@@ -1,5 +1,34 @@
 require "value_object/version"
+require "activesupport/core_ext/class/attribute"
 
-module ValueObject
-  # Your code goes here...
+class ValueObject
+  class_attribute :fields
+  self.fields = []
+
+  class << self
+    def has_fields(*args)
+      attr_reader *args
+      self.fields += args
+    end
+  end
+
+  def fields
+    self.class.fields
+  end
+
+  def initialize(*args)
+    if args.length == 1 && args[0].is_a?(Hash)
+      fields.each { |f| instance_variable_set "@#{f}", args[0][f] }
+    else
+      fields.each_with_index { |f, idx| instance_variable_set "@#{f}", args[idx] }
+    end
+  end
+
+  def ==(other)
+    fields.all? { |f| send(f) == other.send(f) }
+  end
+
+  def empty?
+    fields.all? { |f| send(f).nil? }
+  end
 end

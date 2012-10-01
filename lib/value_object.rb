bad2b43b05e43cd064b8ac70d8d6cc3740391e8c
Harry Garrood <hdgarrood@gmail.com>
2012-10-02 00:14:09 +0100

    fix bugs

--- lib/value_object.rb
+++ lib/value_object.rb
@@ -1,34 +1,36 @@
 require "value_object/version"
-require "activesupport/core_ext/class/attribute"
+require "active_support/core_ext/class/attribute"
 
-class ValueObject
-  class_attribute :fields
-  self.fields = []
+module ValueObject
+  class Base
+    class_attribute :fields
+    self.fields = []
 
-  class << self
-    def has_fields(*args)
-      attr_reader *args
-      self.fields += args
+    class << self
+      def has_fields(*args)
+        attr_reader *args
+        self.fields += args
+      end
     end
-  end
 
-  def fields
-    self.class.fields
-  end
-
-  def initialize(*args)
-    if args.length == 1 && args[0].is_a?(Hash)
-      fields.each { |f| instance_variable_set "@#{f}", args[0][f] }
-    else
-      fields.each_with_index { |f, idx| instance_variable_set "@#{f}", args[idx] }
+    def fields
+      self.class.fields
     end
-  end
 
-  def ==(other)
-    fields.all? { |f| send(f) == other.send(f) }
-  end
+    def initialize(*args)
+      if args.length == 1 && args[0].is_a?(Hash)
+        fields.each { |f| instance_variable_set "@#{f}", args[0][f] }
+      else
+        fields.each_with_index { |f, idx| instance_variable_set "@#{f}", args[idx] }
+      end
+    end
 
-  def empty?
-    fields.all? { |f| send(f).nil? }
+    def ==(other)
+      fields.all? { |f| send(f) == other.send(f) }
+    end
+
+    def empty?
+      fields.all? { |f| send(f).nil? }
+    end
   end
 end
